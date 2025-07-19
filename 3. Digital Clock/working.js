2eaf37014dfbc6afe791bc896c626648c0e3e9d1
adityasingh2929 <2022aditya29@gmail.com>
2025-07-19 13:26:06 +0530

    Made a local digital clock.

--- 3. Digital Clock/working.js
+++ 3. Digital Clock/working.js
@@ -1,1 +1,18 @@
 // JS Code to be written here.
+
+const clock = document.getElementById('clock');
+
+let date = new Date();
+// Now every time the page reloads, this will run and show the latest time.
+console.log(date.toLocaleTimeString());
+
+// But we want to show the latest time all the time, like all the time the clock should be getting updated.
+// So we'll use: 'setInterval' method, which tells that in which intervals, we've to run the given method again and again.
+
+// second parameter is time in  milliseconds.
+setInterval(function(){
+    let date = new Date();
+    // console.log(date.toLocaleTimeString());
+    // Note that date.toLocaleTimeString() method was responsible for us to show the latest time.
+    clock.innerHTML = date.toLocaleTimeString();
+},1000)
